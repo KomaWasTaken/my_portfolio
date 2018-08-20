707d6c970f6eb8594f306a5ccc77e20ffb608dc6
KomaWasTaken <valentin.gaillard-21@outlook.fr>
2018-08-20 16:59:18 +0200

    Ajout partie back-office, upload fonctionnel, ajout footer, burger, ancres, smooth scroll

--- public/index/js/script.js
+++ public/index/js/script.js
@@ -7,6 +7,7 @@ $(function() {
         if (projscroll >= projectsTop) {
             span.removeClass("absolute");
             span.addClass("fixed");
+            span.addClass("z-index-2");
             span.addClass('top-0');
         } else{
             span.removeClass("fixed");
@@ -16,3 +17,46 @@ $(function() {
     }));
   
 });
+
+$(function() {
+
+    var width = screen.width;
+    if ( width > 576 )
+    {
+        var header = $(".navbar");
+        var links = $(".links");
+        $(window).scroll(function() {    
+            var scroll = $(window).scrollTop();
+            if (scroll >= 50) {
+                header.addClass("scrolled_nav");
+                links.addClass("scrolled_links")
+            } else {
+                header.removeClass("scrolled_nav");
+                links.removeClass("scrolled_links")
+            }
+        });
+    }
+    
+
+    var hamburger = document.querySelector(".hamburger");
+    // On click
+    hamburger.addEventListener("click", function() {
+      // Toggle class "is-active"
+      hamburger.classList.toggle("is-active");
+      hamburger.classList.toggle('color-1');
+      $('#navigation').fadeToggle(500);
+      $('#logo').fadeToggle(500);
+    });
+}); 
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+    
